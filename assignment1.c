7ab4f7824c389a3affe9c5db6e74e9f81c99131a
Jon kehoe <67958536+jonkehoe@users.noreply.github.com>
2021-11-16 10:55:28 +0000

    Update assignment1.c

--- assignment1.c
+++ assignment1.c
@@ -1,4 +1,10 @@
+/* maths quizz game using 1 to 4 optiobs */
+/* By Jon Kehoe */
+/* Date: 12/11/2021 */
+
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main()
 {
@@ -8,9 +14,8 @@ int main()
     int wrong = 0;
     int round = 0;
     int answer = 0;
-    int ran_num1 = 0;
-    int ran_num2 = 0;
-    int ran_num3 = 0;
+    int a, b;
+    int total = 0;
 
     do
     {
@@ -19,54 +24,65 @@ int main()
         printf("3 - Display Stats\n");
         printf("4 - Exit Game\n");
         scanf("%d", &num);
-        // srand(time(NULL));
-        // ran_num1 = (rand() % 10) + 1;
-        // ran_num2 = (rand() % 10) + 1;
-        // ran_num3 = (rand() % 10) + 1;
 
         if (num == 1)
         {
+            // reset correct and wrong to 0
             correct = 0;
             wrong = 0;
             printf("enter amount of rounds : ");
             scanf("%d", &round);
             
-        }// end if
+            //error checks for round smaller than 5
+            if(round <= 5)
+            {
+                continue;
+            }// end if
+            else
+            {
+                printf("numbers entered not valid\n");
+            }// end else
+
+        } // end if
         else if (num == 2)
         {
             // for loop to display n number of equations
             for (i = 0; i < round; i++)
             {
-                printf("8/2+4 : ");
+                a = (rand() % 10) + 1;
+                b = (rand() % 10) + 1;
+                total = a * b;
+                srand(time(NULL));
+                printf("%d x %d : ", a, b);
                 scanf("%d", &answer);
                 //check to see if right answer
-                if (answer == 8)
+                if (answer == total)
                 {
                     printf("correct\n");
                     correct++;
-                }//end if
+                } //end if
                 //check for wrong answer
                 else
                 {
-                    printf("wrong! answer = 8\n");
+                    printf("wrong! answer = %d\n", total);
                     wrong++;
-                }// end else
-            }// end for
-        }// end else if
+                } // end else
+            }     // end for
+        }         // end else if
         else if (num == 3 && (correct > 0 || wrong > 0))
         {
             // only display 3 when correct or wrong has a value greater than 0
             printf("Correct = %d\n", correct);
             printf("wrong = %d\n", wrong);
-        }// end else if
+        } // end else if
         else if (num == 3 && (correct < 0 || wrong < 0))
         {
             // else if used to error check input 2 before 3
             printf("must choose option 2 before option 3");
-        }//end else if
+        } //end else if
 
     } while (num != 4);
     printf("Ending game\n");
 
     return 0;
-}//end main
+} //end main
